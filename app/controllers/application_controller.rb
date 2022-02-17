792fedbb088408d686280e9febaee935f62afed8
mack02 <makuuchi23@gmail.com>
2022-02-17 02:15:37 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,14 @@ class ApplicationController < ActionController::Base
   include SessionsHelper  
   include Pagy::Backend
 
-  private
+ private
 
   def require_user_logged_in
     unless logged_in?
       redirect_to login_url
     end
   end
-end
+
+  def counts(user)
+    @count_microposts = user.microposts.count
+  end
